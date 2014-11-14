ab8c6ed15d9557461c26db853736a2d48bb78e0a
Ricardo Simioni <rsimioni@gmail.com>
2014-11-14 19:52:25 -0200

    Updated assets path

--- lib/generators/spree_variant_options/install/install_generator.rb
+++ lib/generators/spree_variant_options/install/install_generator.rb
@@ -2,24 +2,30 @@ module SpreeVariantOptions
   module Generators
     class InstallGenerator < Rails::Generators::Base
       
-      desc "Installs required migrations for spree_essentials"
-      
-      def copy_migrations
-        rake "spree_variant_options:install:migrations"
-      end
+      class_option :auto_run_migrations, :type => :boolean, :default => false
       
       def add_javascripts
-        # append_file "app/assets/javascripts/store/all.js", "//= require spree/frontend/spree_variant_options\n"
-        # should change app/assets to vendor/assets when upgrading to spree 2.2
-        # append_file "vendor/assets/javascripts/spree/frontend/all.js", "//= require spree/frontend/spree_variant_options\n"
+        append_file 'vendor/assets/javascripts/spree/frontend/all.js', "//= require spree/frontend/spree_variant_options\n"
+        append_file 'vendor/assets/javascripts/spree/backend/all.js', "//= require spree/backend/spree_variant_options\n"
       end
 
       def add_stylesheets
-        # inject_into_file "app/assets/stylesheets/store/all.css", "*= require spree/frontend/spree_variant_options\n", :before => /\*\//, :verbose => true
-        # should change app/assets to vendor/assets when upgrading to spree 2.2
-        # inject_into_file "vendor/assets/stylesheets/spree/frontend/all.css", "*= require spree/frontend/spree_variant_options\n", :before => /\*\//, :verbose => true
+        inject_into_file 'vendor/assets/stylesheets/spree/frontend/all.css', " *= require spree/frontend/spree_variant_options\n", :before => /\*\//, :verbose => true
+        inject_into_file 'vendor/assets/stylesheets/spree/backend/all.css', " *= require spree/backend/spree_variant_options\n", :before => /\*\//, :verbose => true
+      end
+      
+      def add_migrations
+        run 'bundle exec rake railties:install:migrations FROM=spree_variant_options'
       end
 
+      def run_migrations
+        run_migrations = options[:auto_run_migrations] || ['', 'y', 'Y'].include?(ask 'Would you like to run the migrations now? [Y/n]')
+        if run_migrations
+          run 'bundle exec rake db:migrate'
+        else
+          puts 'Skipping rake db:migrate, don\'t forget to run it!'
+        end
+      end
     end
   end
 end
